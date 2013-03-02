022ef0fb9d2cc66c425d55f989ddcb8a33b55c32
jeromierussell <jeromierussell@gmail.com>
2013-03-02 12:47:22 -0500

    separate possible drivers from selected queue

--- queue.php
+++ queue.php
@@ -2,21 +2,28 @@
 <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
 <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
 <style>
-    #sortable { list-style-type: none; margin: 0; padding: 0; width: 60%; }
+    #sortable { list-style-type: none; margin: 0; padding: 0; width: 100%; }
     #sortable li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 1.4em; height: 18px; }
     #sortable li span { position: absolute; margin-left: -1.3em; }
+
+    #possible { list-style-type: none; margin: 0; padding: 0; width: 100%; }
+    #possible li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 1.4em; height: 18px; }
+    #possible li span { position: absolute; margin-left: -1.3em; }
+
 </style>
 <script>
     $(function()
     {
         $( "#sortable" ).sortable();
         $( "#sortable" ).disableSelection();
+
+        $("#possible .ui-icon-arrowthick-2-n-s").hide();
     });
 
     function saveOrder()
     {
         var order = "";
-        $(".order").each(function()
+        $("#sortable .order").each(function()
         {
             var add = this.id + ",";
             order += add;
@@ -25,7 +32,24 @@
         var active = $("#useQueue").is(":checked");
 
         $.post("saveorder.php",{ order: order, active: active });
+        alert("Queue Saved!");
     }
+
+    $(document).ready(function()
+    {
+        $("#possible .clickable").live('click', function(event) {
+            var $id = $(this).attr('id');
+            var $returnVal = !$(this).remove().appendTo('#sortable');
+            $("#sortable .ui-icon-arrowthick-2-n-s").show();
+            return $returnVal;
+        });
+
+        $("#sortable .clickable").live('click', function(event) {
+            var $id = $(this).attr('id');
+            var $returnVal = !$(this).remove().appendTo('#possible');
+            return $returnVal;
+        });
+    });
 </script>
 <?php
     $member_id = $_SESSION['USERID'];
@@ -34,6 +58,8 @@
 
     $active = false;
 
+    // duping some query executions here, inefficient, but oh well
+    $in_queue = array();
     if($row != null && $row['id'] != null)
     {
         $queue_sql = "select d.name, d.id from queues q, drivers d where q.member_queue_id = " . $row['id'] . " and d.id = q.driver_id order by q.position";
@@ -41,26 +67,51 @@
         {
             $active = true;
         }
-    }
-    else
-    {
-        $queue_sql = "SELECT id,name,number,owner,team,make,must_qualify FROM drivers d WHERE d.isactive=1 ORDER BY name ASC";
+
+        $queue_results = mysql_query($queue_sql) or die(mysql_error());
+
+        while($row = mysql_fetch_assoc($queue_results))
+        {
+            $in_queue[$row['id']] = 1;
+        }
+
+        $queue_results = mysql_query($queue_sql) or die(mysql_error());
     }
 
-    $queue_results = mysql_query($queue_sql) or die(mysql_error());
+    $possible_sql = "SELECT id,name,number,owner,team,make,must_qualify FROM drivers d WHERE d.isactive=1 ORDER BY name ASC";
+    $possible_results = mysql_query($possible_sql) or die(mysql_error());
 
 ?>
-<a href = "#" onclick="saveOrder();">Save</a>
+<a href = "#" onclick="saveOrder();" style="font-size:1.5em;">Save</a>
 <br/>
 <br/>
-<input type="checkbox" id="useQueue" name="useQueue" <?php if($active) echo 'checked' ?>>Use pick queue to automatically pick when it is your turn</>
+<input type="checkbox" id="useQueue" name="useQueue" <?php if($active) echo 'checked' ?>><span style="color:#B5AFAE">Use pick queue to automatically pick when it is your turn</span></>
 <br/>
 <br/>
-<ul id="sortable">
-<?php
-    while($row = mysql_fetch_assoc($queue_results))
-    {
-        echo "<li class='ui-state-default order' id='" . $row['id'] . "'><span class='ui-icon ui-icon-arrowthick-2-n-s'></span>" . $row['name'] . "</li>";
-    }
-?>
-</ul>
+<div>
+    <div id='possibleDiv' style="vertical-align: top;display: inline-block; width:40%">
+        <span style="color: white;font-size: 1.5em;margin-left: 5px;">All Drivers</span>
+        <ul id="possible">
+            <?php
+                while($row = mysql_fetch_assoc($possible_results))
+                {
+                    if($in_queue[$row['id']] != 1)
+                    {
+                        echo "<li class='ui-state-default order clickable' id='" . $row['id'] . "'><span class='ui-icon ui-icon-arrowthick-2-n-s'></span>" . $row['name'] . "</li>";
+                    }
+                }
+            ?>
+        </ul>
+    </div>
+    <div id='sortableDiv' style="vertical-align: top;display: inline-block; width:40%;float:right">
+        <span style="color: white;font-size: 1.5em;margin-left: 5px;">Your Queue</span>
+        <ul id="sortable">
+            <?php
+                while($row = mysql_fetch_assoc($queue_results))
+                {
+                    echo "<li class='ui-state-default order' id='" . $row['id'] . "'><span class='ui-icon ui-icon-arrowthick-2-n-s'></span>" . $row['name'] . "</li>";
+                }
+            ?>
+        </ul>
+    </div>
+</div>
